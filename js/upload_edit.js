a36d2311e7e9a00b07e20fa736d01681f54b3d52
ari4325 <ari4325@live.com>
2022-09-25 17:43:54 +0530

    local changes

--- js/upload_edit.js
+++ js/upload_edit.js
@@ -60,40 +60,73 @@ save.addEventListener('click', async () => {
         ]
     }
 
-    axios
-    .post("https://livepeer.studio/api/stream", {
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': 'Bearer e608f0f9-338f-431a-8538-a798b2d8c8f5',
+    const res = {
+        "lastSeen": 0,
+        "isActive": false,
+        "record": false,
+        "suspended": false,
+        "sourceSegments": 0,
+        "transcodedSegments": 0,
+        "sourceSegmentsDuration": 0,
+        "transcodedSegmentsDuration": 0,
+        "sourceBytes": 0,
+        "transcodedBytes": 0,
+        "profiles": [
+        {
+            "name": "720p",
+            "bitrate": 2000000,
+            "fps": 30,
+            "width": 1280,
+            "height": 720
         },
-        data: data,
-    })
-    .then(async (res) => {
-        res = res.data;
-        console.log(res);
+        {
+            "name": "480p",
+            "bitrate": 1000000,
+            "fps": 30,
+            "width": 854,
+            "height": 480
+        },
+        {
+            "name": "360p",
+            "bitrate": 500000,
+            "fps": 30,
+            "width": 640,
+            "height": 360
+        }
+        ],
+        "name": "sample1",
+        "kind": "stream",
+        "userId": "0e0b0199-4711-4e5a-8a87-e0f9c65b2d94",
+        "renditions": {},
+        "id": "87898b1f-ad84-49ab-a57d-ec8dcbacd408",
+        "createdAt": 1664105748595,
+        "streamKey": "8789-2ee7-5e0v-zgr3",
+        "playbackId": "87896kirdzvyztz9",
+        "createdByTokenName": "AstterLive",
+        "multistream": {
+            "targets": []
+        }
+    }
 
-        const streamData = [res['name'], res['id']];
-        const profiles = res['profiles'];
-        const profileData = [[profiles[0].name, profiles[0].bitrate+"", profiles[0].fps+"", profiles[0].width+"", profiles[0].height+""], [profiles[1].name, profiles[1].bitrate+"", profiles[1].fps+"", profiles[1].width+"", profiles[1].height+""], [profiles[2].name, profiles[2].bitrate+"", profiles[2].fps+"", profiles[2].width+"", profiles[2].height+""]]
-        
-        const fnData = await astterlive.methods.createStream(streamData, profileData).encodeABI();
+    const streamData = [res['name'], res['id']];
+    const profiles = res['profiles'];
+    const profileData = [[profiles[0].name, profiles[0].bitrate+"", profiles[0].fps+"", profiles[0].width+"", profiles[0].height+""], [profiles[1].name, profiles[1].bitrate+"", profiles[1].fps+"", profiles[1].width+"", profiles[1].height+""], [profiles[2].name, profiles[2].bitrate+"", profiles[2].fps+"", profiles[2].width+"", profiles[2].height+""]]
+    
+    const fnData = await astterlive.methods.createStream(streamData, profileData).encodeABI();
 
-        web3.eth.sendTransaction({
-            from: accounts[0],
-            to: contract_address,
-            data: fnData,
-        })
-        .on('transactionHash', function(hash){
-            console.log(hash);
-        })
-        .on('receipt', function(receipt){
-            console.log(receipt);
-        })
-        .on('confirmation', function(confirmationNumber, receipt){ 
-        })
-        .on('error', console.error);
+    web3.eth.sendTransaction({
+        from: accounts[0],
+        to: contract_address,
+        data: fnData,
     })
-    .catch((error) => {
-        console.log(error);
-    });
+    .on('transactionHash', function(hash){
+        console.log(hash);
+    })
+    .on('receipt', function(receipt){
+        console.log(receipt);
+    })
+    .on('confirmation', function(confirmationNumber, receipt){ 
+    })
+    .on('error', console.error);
+
 })
