f15d8d13792b56b3c89b1cb01925401e61c0bce9
Arishmit Ghosh <ari4325@live.com>
2022-09-25 17:33:11 +0530

    Update upload_edit.js

--- js/upload_edit.js
+++ js/upload_edit.js
@@ -30,7 +30,7 @@ load();
 save.addEventListener('click', async () => {
     const headers = {
         'Content-Type': 'application/json',
-        'Authorization': 'Bearer e608f0f9-338f-431a-8538-a798b2d8c8f5',
+        'Authorization': 'Bearer 81b64de2-c0ef-4d95-a066-6dacbaf6f427',
     }
 
     const data = {
@@ -62,11 +62,8 @@ save.addEventListener('click', async () => {
 
     axios
     .post("https://livepeer.studio/api/stream", {
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': 'Bearer 81b64de2-c0ef-4d95-a066-6dacbaf6f427',
-        },
-        data: data,
+        headers,
+        data,
     })
     .then(async (res) => {
         res = res.data;
