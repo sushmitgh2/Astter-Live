347a52e44482004f5a481838d52db599000e5352
SuyashMatanhelia <matanhelia15.s@gmail.com>
2022-09-25 19:13:49 +0530

    firebase integrated with stream

--- js/stream_page.js
+++ js/stream_page.js
@@ -3,6 +3,9 @@ const button = document.getElementById("button");
 video.volume = 0;
 const { Client } = webRTMP;
 
+web3 = new Web3(window.ethereum);
+addr = await web3.eth.getAccounts()[0];
+
 var url = window.location.href;
 var url = new URL(url);
 var id = url.searchParams.get("id");
@@ -44,6 +47,26 @@ button.onclick = () => {
     }
     const client = new Client();
     const session = client.cast(stream, streamKey);
+
+    const name = data['name'];
+    var firebaseConfig = {
+      apiKey: "AIzaSyBiT5tAohBkKSdufROFxE3hnnplpDhgeHE",
+      authDomain: "aster-live.firebaseapp.com",
+      databaseURL: "https://aster-live-default-rtdb.firebaseio.com",
+      projectId: "aster-live",
+      storageBucket: "aster-live.appspot.com",
+      messagingSenderId:"517213286156",
+      appId: "1:517213286156:web:d2d1a3757cc2af92dfe329",
+    };
+    firebase.initializeApp(firebaseConfig);
+    const db = firebase.database();
+    
+      db.ref("live_streams/"+id).set({
+        id,
+        name,
+        addr,
+      });
+
 
     session.on("close", () => {
       console.log("Stream stopped.");
